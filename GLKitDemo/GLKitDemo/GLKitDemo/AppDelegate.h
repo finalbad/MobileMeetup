d1e5caf4ff7b943b388c2471ec0b183f5fb271bc
boot <dsweetman@mirada.com>
2012-10-12 10:47:30 -0700

    Added GLKit Demo.

---
+++ GLKitDemo/GLKitDemo/GLKitDemo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  GLKitDemo
+//
+//  Created by David Sweetman on 10/10/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
