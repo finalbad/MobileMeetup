9809bea0cfa03f9e2ae1c5fd01e871d41a4123a5
Joseph Kim <joe.kim@gmail.com>
2012-10-12 17:58:52 -0700

    Invert shader.

---
+++ Cocos2dShaderDemo/CocosShaderDemo/CocosShaderDemo/HAPShaderInvert.h
@@ -0,0 +1,16 @@
+//
+//  HAPShaderInvert.h
+//  CocosShaderDemo
+//
+//  Created by Joseph Kim on 10/12/12.
+//  Copyright (c) 2012 Happy Dojo. All rights reserved.
+//
+
+#import "HAPShader.h"
+#import "cocos2d.h"
+
+@interface HAPShaderInvert : HAPShader
+
++ (CCGLProgram *)loadShader;
+
+@end
--- Cocos2dShaderDemo/CocosShaderDemo/CocosShaderDemo/HAPShaderTestLayer.m
+++ Cocos2dShaderDemo/CocosShaderDemo/CocosShaderDemo/HAPShaderTestLayer.m
@@ -10,6 +10,7 @@
 #import "cocos2d.h"
 #import "HAPShader.h"
 #import "HAPShaderGrayscale.h"
+#import "HAPShaderInvert.h"
 @implementation HAPShaderTestLayer
 
 - (id)init;
@@ -21,6 +22,13 @@
         sprite.shaderProgram = [HAPShaderGrayscale loadShader];
         sprite.position = ccp(100, 100);
         [self addChild:sprite];
+
+        CCSprite *sprite2 = [[CCSprite alloc] initWithFile:@"Icon-Small.png"];
+        sprite2.shaderProgram = [HAPShaderInvert loadShader];
+        sprite2.position = ccp(200, 200);
+        [self addChild:sprite2];
+
+
         self.isTouchEnabled = YES;
     }
     return self;
